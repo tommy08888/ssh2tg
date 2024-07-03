f59889a23c70848b7ac90530ef8b3fe225544cfc
tommy08888 <135397378+tommy08888@users.noreply.github.com>
2024-07-03 10:27:53 +0800

    Update ssh2tg.sh

--- ssh2tg.sh
+++ ssh2tg.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# 检查是否安装jq模块，若未安装则自动安装
-# (这一部分可以完全删除，因为不需要安装 jq 了)
-# if ! command -v jq &> /dev/null; then
-#     echo "jq is not installed. Installing jq..."
-#     if command -v apt-get &> /dev/null; then
-#         sudo apt-get update
-#         sudo apt-get install -y jq
-#     elif command -v yum &> /dev/null; then
-#         sudo yum install -y epel-release
-#         sudo yum install -y jq
-#     else
-#         echo "Error: Unable to install jq. Please install jq manually."
-#         exit 1
-#     fi
-# fi
-#我不需要登录后提示所以不装jq
-
 
 # 登录计数文件路径
 count_file="/var/log/ssh_login_count.log"
@@ -29,15 +12,11 @@ else
     count=$(cat $count_file)
 fi
 
-# 显示当前登录次数，并将次数加一
-echo "当前登录次数: $count"
+# 将登录次数加一
 count=$((count+1))
 
 # 将新的登录次数写入计数文件
 echo $count > $count_file
-
-
-
 
 # Telegram Bot相关信息
 TELEGRAM_BOT_TOKEN=replacetoken
@@ -47,11 +26,10 @@ CHAT_ID=replaceid
 IP=$(echo $SSH_CONNECTION | awk '{print $1}')
 TIME=$(date +"%Y年%m月%d日 %H:%M:%S")
 # 查询IP地址对应的地区信息
-#LOCATION=$(curl -s https://ipapi.co/$IP/json/ | jq -r '.city')
- LOCATION=$(curl -s "http://opendata.baidu.com/api.php?query=$IP&co=&resource_id=6006&oe=utf8&format=json" | jq -r '.data[0].location')
+LOCATION=$(curl -s "http://opendata.baidu.com/api.php?query=$IP&co=&resource_id=6006&oe=utf8&format=json" | sed -n 's/.*"location":"\([^"]*\)".*/\1/p')
 # 获取当前用户名
- USERNAME=$(whoami)
- HOSTNAME=$(hostname)
+USERNAME=$(whoami)
+HOSTNAME=$(hostname)
 # 发送Telegram消息
 MESSAGE="登录信息：
 登录机器：$HOSTNAME
@@ -63,6 +41,9 @@ MESSAGE="登录信息：
 
 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESSAGE" >> /root/nohupssh2tg.out 2>&1 & disown
 
-#echo "您此台机器最后5次的登录记录如下！"
-#cat nohupssh2tg.out |jq '.result.text'|tail -5|sort -r|tr -d 'n''"'|sed 's/\\//g'|nl -w 2 -s '、'
-#注释了登录的提示
+# 不显示登录次数
+# echo "当前登录次数: $count"
+
+# 移除读取最近5条记录的部分，确保脚本中没有任何 `jq` 的调用
+# echo "您此台机器最后5次的登录记录如下！"
+# cat nohupssh2tg.out | jq '.result.text' | tail -5 | sort -r | tr -d 'n''"' | sed 's/\\//g' | nl -w 2 -s '、'
